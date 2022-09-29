1ee76c8172611f4d9f11e5b97a4ce231988d462b
DiegoDeLaFlor <89218185+DiegoDeLaFlor@users.noreply.github.com>
2022-09-29 15:24:01 -0500

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script>
+
+export default {
+  name: "App"
+
+};
+</script>
+
+<template>
+  <nav class="flex flex-row gap-4"  >
+    <router-link to="/">Go to Home</router-link>
+    <router-link to="/product">Product</router-link>
+    <router-link to="/comment">Comment</router-link>
+    <router-link to="/post">Post</router-link>
+    <router-link to="/login">Login</router-link>
+  </nav>
+
+  <main>
+    <router-view />
+  </main>
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/Product.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="mb-2">
+    <img src="../assets/myprofile.png" height="200"/>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "Product",
+  data() {
+    return {
+      value:"",
+      categories: [
+        { name: "Lácteos", code: "LA" },
+        { name: "Verduras", code: "VE" },
+        { name: "Frutas", code: "FR" },
+      ],
+    };
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
